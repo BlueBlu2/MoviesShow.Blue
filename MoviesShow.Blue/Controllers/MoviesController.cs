81ff0c4d73ffea163dc5bd658f820250e32f31a9
Blue <eng.abdalrhmn.mohamed@gmail.com>
2021-10-26 21:55:56 +0200

    Finished

--- MoviesShow.Blue/Controllers/MoviesController.cs
+++ MoviesShow.Blue/Controllers/MoviesController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using MoviesShow.Blue.Helpers;
 using MoviesShow.Blue.Models;
 using MoviesShow.Blue.ViewModels;
+using NToastNotify;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -18,15 +19,19 @@ namespace MoviesShow.Blue.Controllers
     {
         private readonly AppDbContext _context;
         private readonly IMapper _mapper;
+        private readonly IToastNotification _toast;
+        private List<string> _allowedExtentions = new List<string> { ".jpg", ".png" };
+        private long _posterSize = 1048576;
 
-        public MoviesController(AppDbContext context, IMapper mapper)
+        public MoviesController(AppDbContext context, IMapper mapper, IToastNotification toast)
         {
             _context = context;
             _mapper = mapper;
+            _toast = toast;
         }
         public async Task<IActionResult> Index()
         {
-            var movies = await _context.Movies.ToListAsync();
+            var movies = await _context.Movies.OrderByDescending(m=>m.Rate).ToListAsync();
             return View(movies);
         }
 
@@ -40,7 +45,7 @@ namespace MoviesShow.Blue.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(MovieFormViewModel movie)
         {
-            await movie.AddGenres(_context); //= await AddGenres(movie);
+            await movie.AddGenres(_context);
             if (!ModelState.IsValid)
             {
                 return View("MovieForm", movie);
@@ -52,14 +57,13 @@ namespace MoviesShow.Blue.Controllers
                 ModelState.AddModelError("Poster", "Please add movie poster");
                 return View("MovieForm", movie);
             }
-            var allowedExtentions = new List<string> { ".jpg", ".png" };
 
-            if (!allowedExtentions.Contains(Path.GetExtension(poster.FileName.ToLower())))
+            if (!_allowedExtentions.Contains(Path.GetExtension(poster.FileName.ToLower())))
             {
                 ModelState.AddModelError("Poster", "Only .jpg , .png are allowed!");
                 return View("MovieForm", movie);
             }
-            if (poster.Length > 1048576)
+            if (poster.Length > _posterSize)
             {
                 ModelState.AddModelError("Poster", "poster cannot be more than 1MB!");
                 return View("MovieForm", movie);
@@ -74,6 +78,7 @@ namespace MoviesShow.Blue.Controllers
             _context.Movies.Add(validatedMovie);
             _context.SaveChanges();
 
+            _toast.AddSuccessToastMessage("Movie created successfully");
             return RedirectToAction(nameof(Index));
         }
 
@@ -92,5 +97,83 @@ namespace MoviesShow.Blue.Controllers
             await editingMovie.AddGenres(_context);
             return View("MovieForm",editingMovie);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(MovieFormViewModel movie)
+        {
+            await movie.AddGenres(_context);
+            if (!ModelState.IsValid)
+                return View("MovieForm", movie);
+            
+            var validatedMovie = await _context.Movies.FindAsync(movie.Id);
+            if (validatedMovie == null)
+                return NotFound();
+
+            var files = Request.Form.Files;
+            var poster = files.FirstOrDefault();
+            if (poster != null)
+            {
+                using var dataStream = new MemoryStream();
+                await poster.CopyToAsync(dataStream);
+                movie.Poster = dataStream.ToArray();
+
+                if (!_allowedExtentions.Contains(Path.GetExtension(poster.FileName.ToLower())))
+                {
+                    ModelState.AddModelError("Poster", "Only .jpg , .png are allowed!");
+                    return View("MovieForm", movie);
+                }
+                if (poster.Length > _posterSize)
+                {
+                    ModelState.AddModelError("Poster", "poster cannot be more than 1MB!");
+                    return View("MovieForm", movie);
+                }
+
+            }
+            else
+            {
+                movie.Poster = validatedMovie.Poster;
+            }
+
+            _mapper.Map(movie, validatedMovie);
+            _context.SaveChanges();
+
+            _toast.AddSuccessToastMessage("Movie updated successfully");
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(Guid? Id)
+        {
+            if(Id == null)
+            {
+                return BadRequest();
+            }
+            var movie = await _context.Movies.Include(m => m.Genre).SingleOrDefaultAsync(m =>m.Id == Id);
+            if(movie == null)
+            {
+                return NotFound();
+            }
+            return View(movie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid? Id)
+        {
+            if (Id == null)
+            {
+                return BadRequest();
+            }
+            var movie = await _context.Movies.FindAsync(Id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+            return Ok();
+        }
+
     }
 }
--- MoviesShow.Blue/Startup.cs
+++ MoviesShow.Blue/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MoviesShow.Blue.Models;
+using NToastNotify;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,8 +31,20 @@ namespace MoviesShow.Blue
                         Configuration.GetConnectionString("DefaultCon")
                         )
                 );
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddNToastNotifyToastr(new ToastrOptions()
+            {
+                ProgressBar = true,
+                PositionClass = ToastPositions.TopRight,
+                PreventDuplicates = true,
+                CloseButton = true
+            });
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddAntiforgery(options =>
+            {
+                options.Cookie.Name = "X-CSRF-TOKEN-MOONGLADE";
+                options.FormFieldName = "CSRF-TOKEN-MOONGLADE-FORM";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,7 +71,7 @@ namespace MoviesShow.Blue
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Movies}/{action=Index}/{id?}");
             });
         }
     }
